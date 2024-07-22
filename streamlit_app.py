009d165a7142e5493365a553a660014e27d4f990
Simranjeet <137194578+Simran-Code-48@users.noreply.github.com>
2024-07-22 16:52:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-from bs4 import BeautifulSoup
-from unidecode import unidecode
+import BeautifulSoup as bs4
+import unidecode as unidecode
 
 # Read CSV data
 @st.cache
